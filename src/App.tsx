92fb7b7f9554d9d23c441e821ec8529952013edb
Alex <adb6799@gmail.com>
2023-05-14 21:11:31 +0500

    Приложение(главная страница)
    Здесь используются все компоненты верхего уровня.
    Кроме того, используется компонент ToastContainer, который отвечает
    за всплывающие сообщения при добавлении города в таблицу.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CitiesList from './components/CitiesList/CitiesList';
+import Header from './components/Header/Header';
+import SearchInput from './components/SearchInput/SearchInput';
+import CitiesTable from './components/Table/CitiesTable';
+import { useAppSelector } from './hooks';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { showTable } = useAppSelector((store) => store.table);
+	return (
+		<main>
+			<ToastContainer position="top-center" autoClose={2000} />
+			<Header />
+			<SearchInput />
+			<CitiesList />
+			{showTable && <CitiesTable />}
+		</main>
+	);
 }
 
 export default App;
